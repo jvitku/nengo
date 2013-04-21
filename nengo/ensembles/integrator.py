7058bf1bd79fc5ee5df6dac0e533babef31f91f8
Chris <celiasmi@v1409-wn-172-21-38-125.campus-dynamic.uwaterloo.ca>
2013-04-21 14:25:47 -0400

    Updated integrator template

--- nengo/ensembles/integrator.py
+++ nengo/ensembles/integrator.py
@@ -1,12 +1,7 @@
+from ..connection import gen_transform
+from ..networks import array
 
-
-
-    
-import numeric
-from java.util import ArrayList
-from java.util import HashMap
-
-def make(network, name='Integrator', neurons=100, dimensions=1, 
+def make(network, input, name='Integrator', neurons=100, dimensions=1, 
          tau_feedback=0.1, tau_input=0.01, scale=1):
     """This constructs an integrator of the specified number of dimensions. 
     It requires an input of that number of dimensions after construction.
@@ -26,42 +21,24 @@ def make(network, name='Integrator', neurons=100, dimensions=1,
     :param float scale:
         A scaling value for the input (controls the rate of integration)
     """
-    net = check_parameters(network, name, neurons, dimensions, tau_feedback, 
+    net = _check_parameters(network, name, neurons, dimensions, tau_feedback, 
                            tau_input, scale)
     
     if (dimensions<8):
         integrator=net.make(name, neurons, dimensions)
     else:
-        integrator=net.make_array(name, int(neurons/dimensions),dimensions, quick=True)
-    net.connect(integrator,integrator,pstc=tau_feedback)
-    integrator.addDecodedTermination('input',numeric.eye(dimensions)*tau_feedback*scale,tau_input,False)
-    if net.network.getMetaData("integrator") == None:
-        net.network.setMetaData("integrator", HashMap())
-    integrators = net.network.getMetaData("integrator")
+        integrator=array.make(name, int(neurons/dimensions), dimensions)
+    
+    recurrent_connection = net.connect(integrator, integrator, 
+                                       pstc=tau_feedback)
+    
+    eye=gen_transform(dimensions, dimensions)
+    input_connection = net.connect(input, integrator, eye*tau_feedback*scale, 
+                                   tau_input)
+    
+    return integrator, recurrent_connection, input_connection
 
-    integrator=HashMap(6)
-    integrator.put("name", name)
-    integrator.put("neurons", neurons)
-    integrator.put("dimensions", dimensions)
-    integrator.put("tau_feedback", tau_feedback)
-    integrator.put("tau_input", tau_input)
-    integrator.put("scale", scale)
-
-    integrators.put(name, integrator)
-
-    if net.network.getMetaData("templates") == None:
-        net.network.setMetaData("templates", ArrayList())
-    templates = net.network.getMetaData("templates")
-    templates.add(name)
-
-    if net.network.getMetaData("templateProjections") == None:
-        net.network.setMetaData("templateProjections", HashMap())
-    templateproj = net.network.getMetaData("templateProjections")
-    templateproj.put(name, name)
-
-    integrator.addDecodedTermination('input', numeric.eye(dimensions)*tau_feedback*scale, tau_input, False)
-
-def check_parameters(network, name, neurons, dimensions, tau_feedback, 
+def _check_parameters(network, name, neurons, dimensions, tau_feedback, 
                      tau_input, scale):
     if isinstance(network, str):
         net = Model.get(network, None)
